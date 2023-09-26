684fa6a254955db2a2ba1d5816d4923f162a34bd
JustWaveThings <justwavethings@proton.me>
2023-09-26 12:31:43 -0400

    display unpublished posts

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,24 @@
-import { useLoaderData } from 'react-router-dom';
+import { useLoaderData, Link } from 'react-router-dom';
 
 const Home = () => {
-  const { title } = useLoaderData();
+  const posts = useLoaderData();
+
   return (
     <div className='element'>
       <h1>Blog Home</h1>
-      {{ title } && <p>{title}</p>}
+      {posts && (
+        <ul>
+          {posts.map(post => (
+            <li key={post._id}>
+              <Link to={`/posts/${post._id}`}>{post.title}</Link>
+              <p>{post.subtitle}</p>
+              <p>{post.created_timestamp}</p>
+              <p>{post.comment_count}</p>
+              <p>{post.post_age_created}</p>
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 };
