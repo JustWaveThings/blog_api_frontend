61116451c1a78fbe9905d2194d77c4e7dbdaf0f9
JustWaveThings <justwavethings@proton.me>
2023-09-26 17:02:13 -0400

    Basic post component mostly fleshed out

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -4,7 +4,39 @@ function Post() {
   const post = useLoaderData();
   console.log(post);
 
-  return <div className='element'>{post?.title}</div>;
+  return (
+    <div className='element'>
+      <p>Title: {post.title}</p>
+      <p>Subheading: {post.subtitle}</p>
+      <p>Created: {post.created_timestamp_formatted}</p>
+      <p>{post.post_age_created} days old</p>
+      <p>
+        {post.likes === 0
+          ? 'Be the first person to like this post.'
+          : post.likes === 1
+          ? `1 like`
+          : `${post.likes} likes`}
+      </p>
+      <br />
+      <p>{post.body}</p>
+
+      {/*  <p>Comments: </p>
+      {post.comments.map(comment => (
+        <div key={comment._id}>
+          <p>{comment.body}</p>
+          <p>{comment.created_timestamp_formatted}</p>
+          <p>{comment.comment_age_created} days old</p>
+          <p>
+            {comment.likes === 0
+              ? 'Be the first person to like this comment.'
+              : comment.likes === 1
+              ? `1 like`
+              : `${comment.likes} likes`}
+          </p>
+        </div>
+      ))} */}
+    </div>
+  );
 }
 
 export default Post;
