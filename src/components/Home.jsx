2fd3c6e2582cf355abe2471bac154e5b21ac8385
JustWaveThings <justwavethings@proton.me>
2023-09-26 15:44:50 -0400

    Update field names

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -11,10 +11,10 @@ const Home = () => {
           {posts.map(post => (
             <li key={post._id}>
               <Link to={`/posts/${post._id}`}>{post.title}</Link>
-              <p>Subtitle: {post.subtitle}</p>
-              <p>Date Created: {post.created_timestamp_formatted}</p>
-              <p>Comment count: {post.comment_count}</p>
-              <p>Post age: {post.post_age_created} Days</p>
+              <p>Subhead: {post.subtitle}</p>
+              <p>Created: {post.created_timestamp_formatted}</p>
+              <p>Comments: {post.comment_count}</p>
+              <p>Age: {post.post_age_created} Days</p>
               <p>Likes: {post.likes}</p>
             </li>
           ))}
