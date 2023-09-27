e3cfdf26a161fe764db84922bd126485ad573f3b
JustWaveThings <justwavethings@proton.me>
2023-09-27 16:59:07 -0400

    Add ScrollRestoration React Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import Header from './components/Header';
 import Footer from './components/Footer';
 
-import { Outlet } from 'react-router-dom';
+import { Outlet, ScrollRestoration } from 'react-router-dom';
 
 function App() {
   return (
@@ -9,6 +9,7 @@ function App() {
       <Header />
       <Outlet />
       <Footer />
+      <ScrollRestoration />
     </>
   );
 }
