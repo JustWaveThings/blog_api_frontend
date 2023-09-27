031f36290efd45dd1348b9ae84a315e6ed6eef7b
JustWaveThings <justwavethings@proton.me>
2023-09-27 17:08:20 -0400

    Remove console.log

--- src/loaders/postLoader.js
+++ src/loaders/postLoader.js
@@ -1,5 +1,4 @@
 async function postLoader(req) {
-  console.log(req.params.id);
   const res = await fetch(`http://localhost:3000/posts/${req.params.id}`);
   const data = await res.json();
   return data;
