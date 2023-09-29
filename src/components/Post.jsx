c1a815de728e8cbd0506c3afdfc1c9609af95951
JustWaveThings <justwavethings@proton.me>
2023-09-29 11:55:42 -0400

    Basic styling improvements / unescape response for comments

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,8 +1,9 @@
 import { useLoaderData, Link } from 'react-router-dom';
+import CommentForm from './CommentForm';
+import validator from 'validator';
 
 function Post() {
   const post = useLoaderData();
-  console.log(post);
 
   return (
     <div className='element'>
@@ -22,16 +23,19 @@ function Post() {
       </p>
       <br />
       <p>{post.body}</p>
-
+      <br />
       <p>All Comments: </p>
+      <br />
       {post.comment_array.map(comment => (
-        <div key={comment._id}>
-          <p>Commenter: {comment.name}</p>
+        <div className='comment' key={comment._id}>
+          <p>Name: {comment.name}</p>
           <p>
             Posted: {comment?.published_timestamp_formatted} (
             {comment?.comment_age_published} days ago)
           </p>
-          <p>Comment: {comment.body}</p>
+          <p className='textarea'>
+            Comment: {validator.unescape(comment.body)}
+          </p>
           <p>
             {comment.likes === 0
               ? '0 likes'
@@ -43,23 +47,17 @@ function Post() {
           </p>
         </div>
       ))}
-
+      <br />
       <p>Add a Comment: </p>
-      <form
-        method='POST'
-        action={`http://localhost:3000/posts/${post._id}/comment `}>
-        <label htmlFor='name'>Name: </label>
-        <input type='text' id='name' name='name' />
-        <label htmlFor='body'>Comment: </label>
-        <textarea id='body' name='body' />
-        <input type='hidden' name='parent_post' value={post._id} />
-        <button>Submit</button>
-      </form>
+      <br />
+      <CommentForm id={post._id} />
+      <br />
       <nav>
         <Link to='/'>
           <button>Back to Posts</button>
         </Link>
       </nav>
+      <br />
     </div>
   );
 }
