668ef5539322ffae40f831d19d7a27a1ce9e1214
JustWaveThings <justwavethings@proton.me>
2023-10-20 14:39:40 -0400

    fix: add days to render duration correctly

--- src/utils/dateTimeDisplay.js
+++ src/utils/dateTimeDisplay.js
@@ -13,17 +13,20 @@ export const dateTimeDisplay = dateTime => {
       'seconds',
     ])
     .toObject();
+
   if (diff.years > 0) {
     return `${diff.years} ${diff.years < 2 ? 'year ago' : 'years ago'}`;
   } else if (diff.months > 0) {
     return `${diff.months} ${diff.months < 2 ? 'month ago' : 'months ago'}`;
   } else if (diff.weeks > 0) {
     return `${diff.weeks} ${diff.weeks < 2 ? 'week ago' : 'weeks ago'}`;
+  } else if (diff.days > 0) {
+    return `${diff.days} ${diff.dayss < 2 ? 'day ago' : 'days ago'}`;
   } else if (diff.hours > 0) {
     return `${diff.hours} ${diff.hours < 2 ? 'hour ago' : 'hours ago'}`;
   } else if (diff.minutes > 0) {
     return `${diff.minutes} ${diff.minutes < 2 ? 'minute ago' : 'minutes ago'}`;
-  } else if (diff.seconds > 30) {
+  } else if (diff.seconds >= 30) {
     return `${diff.seconds.toFixed(0)} seconds ago`;
   } else {
     return 'Just now';
