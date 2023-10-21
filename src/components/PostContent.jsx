82796dac48597b8744de1afc19137a07d2a1bcc5
JustWaveThings <justwavethings@proton.me>
2023-10-21 18:48:19 -0400

    feat: conditionally truncate text display

--- src/components/PostContent.jsx
+++ src/components/PostContent.jsx
@@ -1,8 +1,20 @@
+/* eslint-disable react/prop-types */
 import validator from 'validator';
+import { Link } from 'react-router-dom';
 
-function PostContent({ post }) {
+function PostContent({ post, truncateAtChar, id }) {
+  const truncate = truncateAtChar > 0 && post.length > truncateAtChar;
+  if (truncate) {
+    post = `${validator.unescape(post).slice(0, truncateAtChar)}`;
+  }
+
   const markup = { __html: validator.unescape(post) };
-  return <div className='textarea' dangerouslySetInnerHTML={markup} />;
+  return (
+    <div className='post-content'>
+      <div className='textarea' dangerouslySetInnerHTML={markup} />
+      {truncate && <Link to={`/posts/${id}`}> ...Read More</Link>}
+    </div>
+  );
 }
 
 export default PostContent;
