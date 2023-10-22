8165c4abd31baee6495eec2b2167c7bf459c44f4
JustWaveThings <justwavethings@proton.me>
2023-10-22 13:49:31 -0400

    feat: add socials to footer with icons

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,30 @@
+import { FaGithub, FaLinkedin, FaEnvelope, FaCode } from 'react-icons/fa';
+
 function Footer() {
-  return <div className='footer'>This is the blog footer</div>;
+  return (
+    <div className='footer'>
+      <a
+        href='https://github.com/justwavethings'
+        target='_blank'
+        rel='noreferrer'>
+        <FaGithub />
+      </a>
+      <a
+        href='https://www.linkedin.com/in/fredlove000/'
+        target='_blank'
+        rel='noreferrer'>
+        <FaLinkedin />
+      </a>
+      <a href='mailto:justwavethings@pm.me' target='_blank' rel='noreferrer'>
+        <FaEnvelope />
+      </a>
+      <a
+        href='https://github.com/justwavethings/blog_api_frontend'
+        target='_blank'
+        rel='noreferrer'>
+        <FaCode />
+      </a>
+    </div>
+  );
 }
 export default Footer;
